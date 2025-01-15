2a47e24ce3bd88f363773e78d1077420d5905970
xyrlan <lobatopedro68@gmail.com>
2025-01-15 14:48:38 -0400

    fix: `&quot;` error

--- app/components/FraseSection/FraseSection.tsx
+++ app/components/FraseSection/FraseSection.tsx
@@ -4,13 +4,13 @@ const FraseSection = () => {
   return (
     <div className='bg-[#b4b4a4] text-xl md:text-3xl font-light text-white tracking-wider flex flex-col items-center justify-center h-[50vh] px-6'>
       <p className='max-w-[500px] text-center'>
-        "Neste século de crescer,
-        <br/>
+        `&quot;`Neste século de crescer,
+        <br />
         Tudo cresce sem parar.
-        <br/>
+        <br />
         As ciências se desdobram
-        <br/>
-        Para o mundo se desdobrar. "
+        <br />
+        Para o mundo se desdobrar. `&quot;`
       </p>
       <span className='text-base tracking-wide my-6'>OLÍMPIO PEREIRA</span>
     </div>
