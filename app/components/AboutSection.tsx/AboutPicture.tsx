77432d971e891a6d3d6e3e6843e87a4184c29aac
xyrlan <lobatopedro68@gmail.com>
2025-02-03 05:33:09 -0400

    update: fotos

--- app/components/AboutSection.tsx/AboutPicture.tsx
+++ app/components/AboutSection.tsx/AboutPicture.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 const AboutPicture = () => {
   return (
     <div className='col-span-6  '>
-          <div className='overflow-hidden'>
-          <Image src={'/olimpiogustavo.jpg'} width={700} height={700} alt='Olimpo Pereira Neto e Gustavo Dourado' className='hover:scale-110 transition-all duration-[5000ms] ease-in-out select-none' />
+          <div className='overflow-hidden '>
+          <Image src={'/olimpiogusta.jpg'} width={700} height={600} alt='Olimpo Pereira Neto e Gustavo Dourado' className='hover:scale-110 transition-all duration-[5000ms] ease-in-out select-none' />
           </div>
-          <p className='text-start max-md:text-sm'>Olimpio Pereira, Gustavo Dourado e Elder Vicente</p>
+          <p className='text-start max-md:text-sm'>Olimpio Pereira e Gustavo Dourado</p>
         </div>
   )
 }
--- app/components/HomeSection/SecondHeroSection.tsx
+++ app/components/HomeSection/SecondHeroSection.tsx
@@ -9,7 +9,7 @@ const SecondHeroSection = () => {
       </div> */}
       
       <div className='container mx-auto overflow-hidden'>
-        <Image src={'/orizona.jpg'} width={1800} height={1200} alt='Orizona Olimpio Pereira Neto' className='hover:scale-110 transition-all duration-[5000ms] ease-in-out select-none' />
+        <Image src={'/orizona.jpeg'} width={1800} height={1200} alt='Orizona Olimpio Pereira Neto' className='hover:scale-110 transition-all duration-[5000ms] ease-in-out select-none' />
       </div>
       <div className='container mx-auto'>
         <p className='text-end max-md:text-sm'>Orizona, Goiás</p>
